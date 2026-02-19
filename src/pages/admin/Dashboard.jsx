ce1350bed0db616c158ca19f616bab53b066bf80
utachicodes <abdoullahaljersi@gmail.com>
2026-02-19 20:07:18 +0000

    Fix: Import Link in Admin Dashboard

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useMemo } from "react";
+import { Link } from "react-router-dom";
 import { useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 import {
